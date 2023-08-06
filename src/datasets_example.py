f96764a3ede38f5540c9ca91af1f3f4238a5b4d6
Greg Tatum <tatum.creative@gmail.com>
2023-08-06 05:55:09 -0500

    Working datasets

---
+++ src/datasets_example.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from datasets import load_dataset_builder, load_dataset
+
+print("Looking up the dataset")
+# Load the ParaCrawl dataset
+ds_builder = load_dataset_builder("para_crawl", "enes")
+print(f" > {ds_builder.info.description}")
+print(f" > {ds_builder.info.homepage}")
+
+# print(ds_builder.info)
+#
+# >>> DatasetInfo(
+# >>>     description="Web-Scale Parallel Corpora for Official European Languages.",
+# >>>     citation="@misc {paracrawl,\n    title  = {ParaCrawl},\n    year   = {2018},\n    url    = {http://paracrawl.eu/download.html.}\n}\n",
+# >>>     homepage="https://paracrawl.eu/releases.html",
+# >>>     license="",
+# >>>     features={"translation": Translation(languages=("en", "es"), id=None)},
+# >>>     post_processed=None,
+# >>>     supervised_keys=SupervisedKeysData(input="en", output="es"),
+# >>>     task_templates=None,
+# >>>     builder_name="para_crawl",
+# >>>     dataset_name="para_crawl",
+# >>>     config_name="enes",
+# >>>     version="1.0.0",
+# >>>     splits={
+# >>>         "train": SplitInfo(
+# >>>             name="train",
+# >>>             num_bytes=6209466040,
+# >>>             num_examples=21987267,
+# >>>             shard_lengths=None,
+# >>>             dataset_name=None,
+# >>>         )
+# >>>     },
+# >>>     download_checksums=None,
+# >>>     download_size=1953839527,
+# >>>     post_processing_size=None,
+# >>>     dataset_size=6209466040,
+# >>>     size_in_bytes=None,
+# >>> )
+
+print("Loading data set")
+dataset = load_dataset("para_crawl", "enes", split="train")
+
+print("Splitting data")
+dataset = dataset.train_test_split(test_size=0.1, shuffle=True, seed=1234)
+
+print(dataset)
+print(dataset["test"])
+
+print("Showing example data")
+for row in dataset["test"]["translation"][0:5]:
+    en = row["en"]
+    es = row["es"]
+    print(f"en: {en}")
+    print(f"es: {es}")
---
+++ src/sentence_tokenization.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+"""
+This file loads the Spanish/English dataset from ParaCrawl, and generates a tokenization
+through SentencePiece.
+
+└── data
+    ├── en.model
+    ├── en.txt
+    ├── en.vocab
+    ├── es.model
+    ├── es.txt
+    └── es.vocab
+"""
+
+from datasets import load_dataset_builder, load_dataset
+import sentencepiece as spm
+from os import path
+
+dataset = None  # Lazily initialized
+
+
+def write_file(output_file, lang):
+    global dataset
+    if path.exists(output_file):
+        print(f"The text file '{output_file}' already exists.")
+    else:
+        if not dataset:
+            print("Loading data set")
+            dataset = load_dataset("para_crawl", "enes", split="train").shuffle()
+
+        print(f"Writing out file '{output_file}'.")
+        with open(output_file, "w", encoding="utf-8") as f:
+            for row in dataset["translation"]:
+                f.write(row[lang] + "\n")
+
+
+data_path = path.abspath(path.join(path.dirname(__file__), "../data"))
+text_en = path.join(data_path, "en.txt")
+text_es = path.join(data_path, "es.txt")
+model_en = path.join(data_path, "en.model")
+model_es = path.join(data_path, "en.model")
+
+write_file(text_en, "en")
+write_file(text_es, "es")
+
+
+def run_sentence_piece(lang):
+    model_path = path.join(data_path, f"{lang}.model")
+    if path.exists(model_path):
+        print(f"The model file already exists: {model_path}")
+    else:
+        print(f"Running the training for {lang}")
+        spm.SentencePieceTrainer.train(
+            input=text_en,
+            model_prefix=f"data/{lang}",
+            vocab_size=5000,
+            input_sentence_size=10000,
+        )
+
+
+run_sentence_piece("en")
+run_sentence_piece("es")
+
+
+def output_sentence_piece(lang):
+    model_path = path.join(data_path, f"{lang}.model")
+    text_path = path.join(data_path, f"{lang}.txt")
+    sp = spm.SentencePieceProcessor()
+    sp.load(model_path)
+
+    print("=============================================================")
+    print(f"Examples for {lang}")
+    print("=============================================================")
+
+    with open(text_path, "r", encoding="utf-8") as file:
+        # Read the first 10 lines
+        for line_number, line in enumerate(file):
+            print("\n")
+            print(line[:-1])
+            print(sp.encode_as_pieces(line))
+            print(sp.encode_as_ids(line))
+            if line_number == 49:
+                break
+
+
+output_sentence_piece("en")
+output_sentence_piece("es")
